e79cdbd59ff0cc9b15408ef83773ba0718e0003d
Sebastian Rios <sebastianriosanchez68@gmail.com>
2019-03-09 10:30:50 -0400

    EMtyvyfrAMEwork instalado

---
+++ ProductosCliente/ProductosCliente/Models/DataContet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductosCliente.Models
+{
+    public class DataContet
+    {
+        public class DataContet : DBContect
+    }
+}
---
+++ ProductosCliente/ProductosCliente/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductosCliente.Models
+{
+    public class Producto
+    {
+        int ProductoID;
+        string nombre;
+        double precio;
+
+    }
+
+}
